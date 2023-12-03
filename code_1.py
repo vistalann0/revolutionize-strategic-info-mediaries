62ad73a9dedf4cdb9a58132a480168f7d50fd579
vistalann0 <146479343+vistalann0@users.noreply.github.com>
2023-12-03 14:42:56 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,43 @@
+    return data;
+
+    for (let i = 0; i < 10; i++) {
+
+function generateRandomData() {
+    });
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    data.forEach(item => {
+    }
+    const data = generateRandomData();
+}
+        console.log("Random Number: " + item);
+main();
+function main() {
+    const data = [];
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+        print(f"Random Number: {item}")
+def main():
+import random
+    return data
+def generate_random_data():
+if __name__ == "__main__":
+
+
+    data = generate_random_data()
+
+    main()if __name__ == "__main__":
+
+
+    main()
+def main():
+def generate_random_data():
+        print(f"Random Number: {item}")
+    return data
+import random
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    data = generate_random_data()
